0d6df8d4a75b5ed38a1e960902f3fc435343fec0
HsuehSheng <a0905678648@gmail.com>
2023-11-05 14:20:45 +0800

    first

---
+++ P18/source/Main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+	char string1[60] = "welcome";
+	char string2[60];
+
+	int len;
+
+	strcpy(string2, string1);
+	printf("string2=%s\n", string2);
+	len = strlen(string2);
+	printf("¦r¦êªø«×¬°%d\n", len);
+
+	system("pause");
+	return 0;
+}
